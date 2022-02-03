49dcefbfd0b77602deb3a920c2e550d5c10394aa
jnssk <97682964+jnssk@users.noreply.github.com>
2022-02-03 15:00:33 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "web-1" {
     #ami = "ami-0d857ff0f5fc4e03b"
     #ami = "${data.aws_ami.my_ami.id}"
     instance_type = "t2.micro"
-    key_name = "splunk"
+    key_name = "keypair"
     subnet_id = aws_subnet.subnets.0.id
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true
